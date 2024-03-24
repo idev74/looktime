ace83f8f3ff542c229b83c9f3de1dd29b054bbbb
idev74 <idevassy77@gmail.com>
2024-03-24 11:48:30 -0700

    added Timer reducers

---
+++ src/app/app.js
@@ -0,0 +1,12 @@
+import { configureStore } from "@reduxjs/toolkit";
+import timersReducer from '../features/timers/timersSlice';
+
+export const store = configureStore({
+	reducer: {
+		timers: timersReducer
+	},
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false
+    })
+})
