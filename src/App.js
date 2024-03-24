df2c57fc1524a65d915aea05ec46c6c4341cb333
idev74 <idevassy77@gmail.com>
2024-03-24 11:52:36 -0700

    added new timer input

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import React from 'react';
 import './App.css';
+import NewTimer from './components/NewTimer';
 
 export default function App() {
     return (
         <div className="App">
+            <NewTimer />
         </div>
     );
 }
